fcb2fcbfdd0ff514046a6b87bda69d1658e3b52d
phroste <stevenhhui@gmail.com>
2018-03-08 21:31:30 -0500

    full crud and 2 tables

--- pets_app/app/controllers/pets_controller.rb
+++ pets_app/app/controllers/pets_controller.rb
@@ -2,39 +2,53 @@ class PetsController < ApplicationController
 
   # get all pets
   def index
-    pets = Pet.all
-    render json: pets
+    @pets = Pet.all
   end
 
-  # get method
+  # get method for individual pet
   def show
-    pet = Pet.find(params[:id])
-    render json: pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def new 
+    @pet = Pet.new
   end
 
   # post method
   def create
-    p "in create. pet_params:", pet_params
-    pet = Pet.create!(pet_params)
-    render json: pet
+    @pet = Pet.new(pet_params)
+
+    if @pet.save
+      redirect_to @pet
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
   end
 
   # put method
-  def update
-    pet = Pet.find(params[:id])
-    pet.update!(pet_params)
-    render json: pet
+  def update 
+    @pet = Pet.find(params[:id])
+
+    if @pet.update(pet_params)
+      redirect_to @pet
+    else
+      render :action => :edit
+      # render 'edit'
+    end
   end
 
   # delete method
   def destroy
-    pet = Pet.find(params[:id])
-    pet.destroy!
-    render plain: "pet DELETED!"
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_url
   end
 
   private
-
   # defining the variable pet_params...
   def pet_params
     params.require(:pet).permit(:pet_name, :pet_specie, :pet_image, :owner_id)
